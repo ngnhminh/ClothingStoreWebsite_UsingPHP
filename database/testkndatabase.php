b1b488365865c679d3d5e7bd7ac5ace76518edcb
vietcuong <nguyencuongpq2004@gmail.com>
2025-03-08 10:06:18 +0700

    kn databasedatabase

---
+++ database/testkndatabase.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+
+$sql = "SELECT * FROM sanpham";
+$result = mysqli_query($conn, $sql);
+echo "Kết Nối Thành Công";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "Tên sản phẩm: " . $row['ten_sanpham'] . "<br>";
+  
+}
+?>
